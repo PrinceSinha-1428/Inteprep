464c197ebf3d633e22c7bc63cc8001bd2e3e6c47
Prince <princesinha629@gmail.com>
2025-03-24 22:37:03 +0530

    routes created

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className='dark'>
-      <body
+    <html lang="en" className='dark' suppressHydrationWarning>
+      <body suppressHydrationWarning
         className={`${monasans.className} antialiased`}
       >
         {children}
