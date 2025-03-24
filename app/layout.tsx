a8c7bed3a7dbd90e031c12221d1ec68a7a7c8732
Prince <princesinha629@gmail.com>
2025-03-24 22:39:59 +0530

    tailwindcss-animate plugin installed

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className='dark' suppressHydrationWarning>
       <body suppressHydrationWarning
-        className={`${monasans.className} antialiased`}
+        className={`${monasans.className} antialiased pattern`}
       >
         {children}
       </body>
