b44645c704323ae9f23d6fefff04f52635548265
Prince <princesinha629@gmail.com>
2025-03-27 04:47:09 +0530

    fixed some minor bugs to prevent errors

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -12,8 +12,8 @@ const page = async () => {
     await getInterviewByUserId(user?.id!),
     await getLatestInterviews({userId:user?.id!})
   ]);
-  const hasPastInterviews = userInterviews?.length > 0;
-  const hasUpcomingInterviews = latestInterviews?.length > 0;
+  const hasPastInterviews = userInterviews?.length! > 0;
+  const hasUpcomingInterviews = latestInterviews?.length! > 0;
   return (
     <>
       <section className='card-cta'>
