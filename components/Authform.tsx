0f27c9f1db6983a1fe80a9ff7dc5a9c3e68d5e58
Prince <princesinha629@gmail.com>
2025-03-28 00:54:51 +0530

    authform

--- components/Authform.tsx
+++ components/Authform.tsx
@@ -37,43 +37,59 @@ const Authform = ({ type }: { type: FormType }) => {
   });
 
   // 2. Define a submit handler.
- async function onSubmit(values: z.infer<typeof formSchema>) {
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     try {
-        if(type === 'sign-up'){
-          const {name,email,password} = values;
-          const userCredentials = await createUserWithEmailAndPassword(auth,email,password)
-          const result = await signUp({
-            uid: userCredentials.user.uid,
-            name: name!,
-            email,
-            password
-          })
-          if(!result?.success){
-            toast.error(result?.message);
-            return;
-          }
-           toast.success("Account Created Successfully, Please Sign in");
-           router.push('/sign-in')
-        }else{
-            const {email,password} = values;
-            const userCredentials = await signInWithEmailAndPassword(auth,email,password);
-            const idToken = await userCredentials.user.getIdToken();
-            if(!idToken){
-              toast.error("Sign in failed");
-              return;
-            }
-            await signIn({
-              email,idToken
-            })
-            toast.success("Sign in successfully");
-           router.push('/')
+      if (type === "sign-up") {
+        const { name, email, password } = data;
+
+        const userCredential = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const result = await signUp({
+          uid: userCredential.user.uid,
+          name: name!,
+          email,
+          password,
+        });
+
+        if (!result.success) {
+          toast.error(result.message);
+          return;
         }
-        
+
+        toast.success("Account created successfully. Please sign in.");
+        router.push("/sign-in");
+      } else {
+        const { email, password } = data;
+
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+        const idToken = await userCredential.user.getIdToken();
+        if (!idToken) {
+          toast.error("Sign in Failed. Please try again.");
+          return;
+        }
+
+        await signIn({
+          email,
+          idToken,
+        });
+
+        toast.success("Signed in successfully.");
+        router.push("/");
+      }
     } catch (error) {
-        console.log(error)
-       toast.error(`There was an error ${error}`)
+      console.log(error);
+      toast.error(`There was an error: ${error}`);
     }
-  }
+  };
   return (
     <div className="card-border lg:min-w-[566px] ">
       <div className="flex flex-col gap-6 card py-14 px-10">
